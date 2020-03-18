c946e82721258349b7da32eef165a12617721fd3
trurtrl <trurtrl@gmail.com>
2020-03-19 01:22:46 +0200

    Init

---
+++ Source/CsvActorMover/CsvActorMoverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CsvActorMoverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CSVACTORMOVER_API ACsvActorMoverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
